203177953aa238ecbb9b3509499fdad949ccbde2
Afoma <antoinetteorji@gmail.com>
2025-07-16 20:24:04 +0100

    refactored all the code

--- foundry-fund-me-f23/src/FundMe.sol
+++ foundry-fund-me-f23/src/FundMe.sol
@@ -7,24 +7,13 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 error FundMe__NotOwner();
 
 contract FundMe {
-    // allow users to send $
-    // have a minimum $ spent $5
     using PriceConverter for uint256;
-    // this will import the library and attach it to
-    // the desired type (functions here) in our contract
-    // PriceConverter to all the uint256 types here
-    // with the `using` and `for`keyword
     address[] public funders;
-    // this is an array of the people/addresses 
-    // that are paying into this contract
     mapping (address => uint256) public addressToAmountFunded;
-    // here we map the paying people/ addresses to the amount
-    // they are paying into the contract
 
     address public immutable i_owner;
     uint256 public constant MINIMUM_USD = 5e18;
     AggregatorV3Interface private s_priceFeed;
-    // 
 
     constructor(address priceFeed) {
         i_owner = msg.sender;
@@ -32,22 +21,14 @@ contract FundMe {
     }
 
     function fund() public payable{
-        // it will receive funds so i make it payable
         require (msg.value.getConversionRate(s_priceFeed) > MINIMUM_USD, "didn't send enough eth");
-        // getConversionRate wraps the msg.value
-        // to make it in terms of usd
-        // msg.value is the amount of eth paid into a contract
         funders.push(msg.sender);
-        // here we push those who have paid into 
-        // an array or list called funders 
-        // that has their addresses
         addressToAmountFunded[msg.sender] += msg.value;
     }
     function getVersion() public view returns(uint256){
         return s_priceFeed.version();
     }
     modifier onlyOwner() {
-        // require(msg.sender == owner, "Must be owner");
         if(msg.sender != i_owner) {revert FundMe__NotOwner();}
         _;
     }
@@ -57,18 +38,6 @@ contract FundMe {
             addressToAmountFunded[funder] = 0;
         }
         funders = new address[](0);
-        // this will reset the funders array to an empty array
-        // new array that starts at index 0
-        
-        /* 
-        ** transfer **
-        payable(msg.sender).transfer(amount);
-        ** send **
-        bool success = payable(msg.sender).send(this).balance;
-        require (success, "No send");
-        */
-
-        // call
         (bool success,) = payable(msg.sender).call{value:address(this).balance}("");
         require(success, "No call");
     }
--- foundry-fund-me-f23/src/PriceConverter.sol
+++ foundry-fund-me-f23/src/PriceConverter.sol
@@ -4,10 +4,7 @@ pragma solidity ^0.8.19;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-        function getData(AggregatorV3Interface priceFeed) internal view returns (uint256) {
-        // this function will return the price of eth
-        // in terms of usd
-        // we will use the chainlink data feed
+        function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
         (
             ,
             int256 answer,// price of eth in terms of usd
@@ -15,22 +12,10 @@ library PriceConverter {
             ,           
         ) = priceFeed.latestRoundData();
         return uint256(answer * 1e10); 
-        // the uin256 that wraps the answer is to convert
-        // int256 to uin256. this method is called type casting
-        // 1e10 is used to convert the price to 18 decimals
     }
     function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns(uint256){
-        // this function will convert the eth amount we got 
-        // in the getData function to usd
-        uint256 ethPrice = getData(priceFeed);
+        uint256 ethPrice = getPrice(priceFeed);
         uint256 ethAmountInUsd = (ethPrice*ethAmount) / 1e18;
-        // in solidity, we multiply before dividing
-        // to avoid precision loss
-        // we divide their result by 1e18 since each of them-
-        // ethPrice and ethAmount are in 18 decimals
-        // and their result will be in 36 decimal places
-        // and this will be too much for our readability
         return ethAmountInUsd;
-
     }
 }
