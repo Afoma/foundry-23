ab87c987d13f11de364c346ef91732c3fa08ba24
Afoma <antoinetteorji@gmail.com>
2025-07-21 15:52:48 +0100

    foundry cheatcodes

--- foundry-fund-me-f23/src/FundMe.sol
+++ foundry-fund-me-f23/src/FundMe.sol
@@ -8,22 +8,22 @@ error FundMe__NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
-    address[] public funders;
-    mapping (address => uint256) public addressToAmountFunded;
+    address[] private s_funders;
+    mapping (address => uint256) private s_addressToAmountFunded;
 
     address public immutable i_owner;
     uint256 public constant MINIMUM_USD = 5e18;
     AggregatorV3Interface private s_priceFeed;
 
-    constructor(address priceFeed) {
+    constructor(address priceFeed) {        
         i_owner = msg.sender;
         s_priceFeed = AggregatorV3Interface(priceFeed);
-    }
+    }                                                                                             
 
     function fund() public payable{
         require (msg.value.getConversionRate(s_priceFeed) > MINIMUM_USD, "didn't send enough eth");
-        funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+        s_addressToAmountFunded[msg.sender] += msg.value;
     }
     function getVersion() public view returns(uint256){
         return s_priceFeed.version();
@@ -33,11 +33,11 @@ contract FundMe {
         _;
     }
     function withdraw() public onlyOwner {
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
+        for(uint256 funderIndex = 0; funderIndex < s_funders.length; funderIndex++){
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
         }
-        funders = new address[](0);
+        s_funders = new address[](0);
         (bool success,) = payable(msg.sender).call{value:address(this).balance}("");
         require(success, "No call");
     }
@@ -46,5 +46,16 @@ contract FundMe {
     }
     fallback() external payable{
         fund();
+    }
+
+    /* 
+    View / Pure functions (Getters)
+     */
+
+    function getAddressToAmountFunded(address fundingAddress) external view returns(uint256){
+        return s_addressToAmountFunded[fundingAddress];
+    }
+    function getFunder(uint256 index) external view returns(address){
+        return s_funders[index];
     }
 }
--- foundry-fund-me-f23/test/FundMeTest.t.sol
+++ foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -6,11 +6,15 @@ import {FundMe} from '../src/FundMe.sol';
 import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
+    address user = makeAddr('user');
+    uint256 SEND_VALUE= 0.1 ether;
     FundMe public fundMe;
+    uint256 STARTING_BALANCE = 10 ether;
 
     function setUp() external{
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(user, STARTING_BALANCE);
     }
     function testMinimumDollarIsFive() public{
         assertEq(fundMe.MINIMUM_USD(), 5e18);
@@ -22,5 +26,15 @@ contract FundMeTest is Test{
         uint256 version = fundMe.getVersion();
         console.log(version);
         assertEq(version, 4);
+    }
+    function testFundFailsWithoutEnoughETH() public{
+        vm.expectRevert();
+        fundMe.fund();
+    }
+    function testFundUpdatesFundDataStructure() public{
+        vm.prank(user);
+        fundMe.fund{value:SEND_VALUE}();
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(user);
+        assertEq(amountFunded, SEND_VALUE);
     }
 }
